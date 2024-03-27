a26b8c00eb5cdae3f2ffc076f2995c9a7c6d72cc
Luca Casamassima <casamassimaluca723@gmail.com>
2024-03-27 14:18:41 +0100

    error modal on login and buttons css fix

--- src/Modals/Login/Login.jsx
+++ src/Modals/Login/Login.jsx
@@ -5,15 +5,16 @@ import Input from "../../Input/Input";
 import styles from "./Login.module.css";
 import { useContext, useState } from "react";
 import { OrderContext } from "../../store/food-order-context";
-
+import ErrorModal from "../Error/ErrorModal";
+import Modal from "../Modal/Modal";
 export default function Login({ setLogin }) {
   const { userCtx, setUserCtx } = useContext(OrderContext);
 
   const [userData, setUserData] = useState({
     name: "",
     password: "",
-
     isLogged: false,
+    error: false,
   });
 
   function getUserDatas(e) {
@@ -23,17 +24,25 @@ export default function Login({ setLogin }) {
   }
 
   function handleLogin() {
-    setUserCtx({
-      name: userData.name,
-      password: userData.password,
-      admin: userData.admin,
-      isLogged: true,
-    });
-    console.log(userCtx);
+    if (userData.name === "" || userData.password === "") {
+      setUserData((prev) => ({ ...prev, error: true }));
+      return;
+    } else {
+      setUserCtx({
+        name: userData.name,
+        password: userData.password,
+        admin: userData.admin,
+        isLogged: true,
+      });
+      console.log(userCtx);
+    }
   }
 
   return (
     <div id={styles.login}>
+      <Modal open={userData.error}>
+        <ErrorModal onClose={() => setLogin(false)} />
+      </Modal>
       {userCtx.isLogged ? (
         <div>
           <h2>Welcome back {userCtx.name}</h2>
